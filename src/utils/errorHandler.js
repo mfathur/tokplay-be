5ea3d59eec83830f53202403c8c5429d00dc6f2f
mfathur <mfathur17@gmail.com>
2023-07-24 13:02:59 +0700

    fix errorHandler

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,6 +1,9 @@
 export const errorHandler = (err, req, res, next) => {
   const errorCode = err.statusCode || 500;
-  const errorMessage = err.message || "Something went wrong";
+  const errorMessage =
+    errorCode === 500
+      ? "Internal server error"
+      : err.message || "Something went wrong";
 
   return res.status(errorCode).json({
     status: "fail",
