ae2909da8ab97e38fe411b843217e09a4d00a2f3
mfathur <mfathur17@gmail.com>
2023-07-25 17:03:08 +0700

    edit commentSchema

--- src/apis/videos/models/comment.js
+++ src/apis/videos/models/comment.js
@@ -5,10 +5,12 @@ export const CommentSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      trim: true,
     },
     comment: {
       type: String,
       required: true,
+      trim: true,
     },
   },
   { timestamps: true }
