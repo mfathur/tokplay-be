69659a4ee4853269c05608eee00cbe4b446d73f1
mfathur <mfathur17@gmail.com>
2023-08-14 14:34:11 +0700

    change dev port

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
   development: {
-    port: 3000,
+    port: 8080,
     mongodb_uri: "mongodb://tokplay-mongo:27017/tokplaydb",
     corsHeader: ["Link", "Content-Disposition"],
     corsOrigin: "*",
